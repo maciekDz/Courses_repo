2c370f71280004899461b2c27c1b77035e8569ad
maciej@dziendziel.com <maciej@dziendziel.com>
2019-04-16 23:58:46 +0200

    Adding Courses Context

---
+++ PraktyczneKursy/PraktyczneKursy/DAL/CoursesContext.cs
@@ -0,0 +1,31 @@
+﻿using PraktyczneKursy.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+
+namespace PraktyczneKursy.DAL
+{
+    public class CoursesContext:DbContext
+    {
+        public CoursesContext() : base("CoursesContext")
+        {
+
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderElement> OrderElements { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+    }
+}
