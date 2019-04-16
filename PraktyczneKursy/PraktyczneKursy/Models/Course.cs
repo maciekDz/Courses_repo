7b4ebe16511cb60d294489bee6eced13982171df
maciej@dziendziel.com <maciej@dziendziel.com>
2019-04-16 23:18:25 +0200

    Course class creation

---
+++ PraktyczneKursy/PraktyczneKursy/Models/Course.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PraktyczneKursy.Models
+{
+    public class Course
+    {
+
+        public int CourseId { get; set; }
+        public int CategotyId { get; set; }
+        public string CourseTitle { get; set; }
+        public int CourseAuthor { get; set; }
+        public DateTime InsertDate { get; set; }
+        public string FileOrPicturePhotoName { get; set; }
+        public string CourseDescription { get; set; }
+        public decimal CoursePrice { get; set; }
+        public bool Bestseller { get; set; }
+        public bool Hidden { get; set; }
+    }
+}
