ede354b5e1bcfbe45fc9959df8ef6ec666f20138
maciej@dziendziel.com <maciej@dziendziel.com>
2019-05-03 15:55:10 +0200

    Installing jquery, creating bundle class, putting searchform on category-courses view. css problem again.

--- PraktyczneKursy/PraktyczneKursy/Controllers/CoursesController.cs
+++ PraktyczneKursy/PraktyczneKursy/Controllers/CoursesController.cs
@@ -17,10 +17,21 @@ namespace PraktyczneKursy.Controllers
             return View();
         }
 
-        public ActionResult List(string categoryName)
+        public ActionResult List(string categoryName, string searchQuery = null)
         {
             var category = db.Categories.Include("Courses").Where(c => c.CategoryName.ToUpper() == categoryName.ToUpper()).Single();
-            var courses = category.Courses.ToList();
+
+            //var courses = category.Courses.ToList();
+            var courses = category.Courses.Where(a => (searchQuery == null ||
+                a.CourseTitle.ToLower().Contains(searchQuery.ToLower()) ||
+                a.CourseAuthor.ToLower().Contains(searchQuery.ToLower())) &&
+                !a.Hidden);
+
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView("_CoursesList", courses);
+            }
+
             return View(courses);
         }
 
@@ -30,15 +41,25 @@ namespace PraktyczneKursy.Controllers
             return View(course);
         }
 
-        [ChildActionOnly] 
-        [OutputCache(Duration =60000)]
+        [ChildActionOnly]
+        [OutputCache(Duration = 60000)]
         public ActionResult CategoryMenu()
         {
-            
+
             var categories = db.Categories.ToList();
             return PartialView("_CategoryMenu", categories);
         }
 
-        
+        public ActionResult CoursesHints(string term)
+        {
+            var courses = db.Courses.Where(a => !a.Hidden && a.CourseTitle.ToLower().Contains(term.ToLower()))
+                    .Take(5).Select(a => new { label = a.CourseTitle });
+
+            return Json(courses, JsonRequestBehavior.AllowGet);
+
+        }
+
+
+
     }
 }
