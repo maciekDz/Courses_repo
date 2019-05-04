392a49696cea1a7967feaaf0331910f501b22de6
maciej@dziendziel.com <maciej@dziendziel.com>
2019-05-04 18:38:58 +0200

    Register+Login

---
+++ PraktyczneKursy/PraktyczneKursy/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using PraktyczneKursy.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PraktyczneKursy.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(RegisterViewModel model)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            else
+            {
+                return RedirectToAction("Index","Home");
+            }
+        }
+    }
+}
---
+++ PraktyczneKursy/PraktyczneKursy/ViewModels/AccountViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PraktyczneKursy.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage ="Please provide e-mail address")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage ="Please provide password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name ="Remember me")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Please provide e-mail address")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please provide password")]
+        [StringLength(30, ErrorMessage = "{0} must have {2} characters.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password",ErrorMessage ="Password and Confirm Password don't match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
