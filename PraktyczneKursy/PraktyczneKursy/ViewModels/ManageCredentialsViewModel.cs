d7839005b395141dcac3b89b18acc8f735c407fd
maciej@dziendziel.com <maciej@dziendziel.com>
2019-05-09 23:02:57 +0200

    Adding change user data and change password functionality

---
+++ PraktyczneKursy/PraktyczneKursy/ViewModels/ManageCredentialsViewModel.cs
@@ -0,0 +1,40 @@
+﻿using PraktyczneKursy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using static PraktyczneKursy.Controllers.ManageController;
+using System.ComponentModel.DataAnnotations;
+
+namespace PraktyczneKursy.ViewModels
+{
+
+    public class ManageCredentialsViewModel
+    {
+        public ChangePasswordViewModel ChangePasswordViewModel { get; set; }
+        public ManageMessageId? Message { get; set; }
+        public UserData UserData { get; set; }
+    }
+
+    public class ChangePasswordViewModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name ="Current Password")]
+        public string OldPassword { get; set; }
+
+
+        [Required]
+        [StringLength(100,ErrorMessage ="The {0} must be at least {2} characters long.",MinimumLength =6)]
+        [DataType(DataType.Password)]
+        [Display(Name ="New Password")]
+        public string NewPassword { get; set; }
+
+        
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm New Password")]
+        [Compare("NewPassword",ErrorMessage ="The new password and confirmation password don't match.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+}
